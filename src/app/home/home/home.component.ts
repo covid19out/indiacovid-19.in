d751e968be6eafc99d936b41a7f7bfee0c538ef6
Amabdas Avhad <ambadas.avhad@aptlogica.com>
2020-03-16 23:45:23 +0530

    Added dashboard card charts

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -14,12 +14,12 @@ export class HomeComponent implements OnInit {
   public barChartOptions: ChartOptions = {
     responsive: true,
   };
-  public barChartLabels: Label[] = ['01 Feb', '02 Mar','03 Mar', '04 Mar', '05 Mar','06 Mar', '07 Mar','08 Mar', '09 Mar','10 Mar','11 Mar', '12 Mar' ,'13 Mar','14 Mar','15 Mar', '16 Mar'];
+  public barChartLabels: Label[] = ['01 Feb', '02 Mar', '03 Mar', '04 Mar', '05 Mar', '06 Mar', '07 Mar', '08 Mar', '09 Mar', '10 Mar', '11 Mar', '12 Mar', '13 Mar', '14 Mar', '15 Mar', '16 Mar'];
   public barChartType: ChartType = 'bar';
   public barChartLegend = true;
   public barChartPlugins = [];
   public barChartData: ChartDataSets[] = [
-    { data: [2, 3, 5, 6, 29, 30, 31,34,39,45,50,71,76,83,95,109,119], label: 'REPORTED SYMPTOMATIC', stack: 'a' },
+    { data: [2, 3, 5, 6, 29, 30, 31, 34, 39, 45, 50, 71, 76, 83, 95, 109, 119], label: 'REPORTED SYMPTOMATIC', stack: 'a' },
     { data: [0, 1, 40, 17, 1, 1, 2,], label: 'CONFIRMED CASES', stack: 'a' },
     { data: [1, 1, 25, 6, 0, 0, 1,], label: ' DISCHARGED', stack: 'a' }
   ];
@@ -40,20 +40,20 @@ export class HomeComponent implements OnInit {
     }
   };
 
-//   public pieChartGenderPlugins = [{
-//   afterLayout: function (chart) {
-//     console.log(chart.legend.legendItems)
-//     chart.legend.legendItems.forEach(
-//       (label,i) => {
-//         console.log("lable",chart.data.datasets[0]);
-//         let value = chart.data.datasets[0].data[label.index];
-//
-//         label.text += ' ' + value;
-//         return label;
-//       }
-//     )
-//   }
-// }];
+  //   public pieChartGenderPlugins = [{
+  //   afterLayout: function (chart) {
+  //     console.log(chart.legend.legendItems)
+  //     chart.legend.legendItems.forEach(
+  //       (label,i) => {
+  //         console.log("lable",chart.data.datasets[0]);
+  //         let value = chart.data.datasets[0].data[label.index];
+  //
+  //         label.text += ' ' + value;
+  //         return label;
+  //       }
+  //     )
+  //   }
+  // }];
 
 
 
@@ -68,19 +68,6 @@ export class HomeComponent implements OnInit {
       position: 'bottom'
     }
   };
-
-//   public pieChartNationalityPlugins = [{
-//   afterLayout: function (chart) {
-//     chart.legend.legendItems.forEach(
-//       (label) => {
-//         let value = chart.data.datasets[0].data[label.index];
-//
-//         label.text += ' ' + value;
-//         return label;
-//       }
-//     )
-//   }
-// }];
 
   //Doughnut Source
   public doughnutSourceChartLabels: Label[] = ['Local Transmission', 'Imported Cases'];
@@ -102,13 +89,26 @@ export class HomeComponent implements OnInit {
   public lineChartPlugins = [];
   //Gender over Time line chart
   public lineChartData: ChartDataSets[] = [
-    { data: [10, 12, 10, 8, 15, 18, 14, 17, 21, 23], label: 'Male' },
-    { data: [ 7,  8,  9, 6, 10, 14, 12, 14, 19, 20], label: 'Female' },
+    { data: [10, 12, 10, 8, 15, 18, 14, 17, 21, 23], label: 'Male', cubicInterpolationMode: 'monotone' },
+    { data: [7, 8, 9, 6, 10, 14, 12, 14, 19, 20], label: 'Female', cubicInterpolationMode: 'monotone' },
   ];
   public lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
   public lineChartOptions: (ChartOptions & { annotation: any }) = {
     responsive: true,
-    annotation: false
+    annotation: false,
+    scales: {
+      xAxes: [
+        {
+          display: false
+        }
+      ],
+      yAxes: [
+        {
+          display: false
+        }
+      ]
+    }
+
   };
   public lineChartColors: Color[] = [
     {
@@ -121,32 +121,28 @@ export class HomeComponent implements OnInit {
     }
   ];
 
-  public pieChartGenderOverTimeOptions: ChartOptions = {
-    scales: {
-       xAxes: [{
-           gridLines: {
-               display:false
-           }
-       }],
-       yAxes: [{
-           gridLines: {
-               display:false
-           }
-       }]
-   }
-};
 
-
-
-//Infection Source over Time
+  //Infection Source over Time
   public lineChartInfectionSourceData: ChartDataSets[] = [
-    { data: [10, 12, 10, 8, 15, 18, 14, 17, 21, 23], label: 'IMPORTED CASE' },
-    { data: [ 0,  0,  0, 6, 10, 20, 17, 22, 35, 40], label: 'LOCAL TRANSMISSION' },
+    { data: [10, 12, 10, 8, 15, 18, 14, 17, 21, 23], label: 'IMPORTED CASE', cubicInterpolationMode: 'monotone' },
+    { data: [0, 0, 0, 6, 10, 20, 17, 22, 35, 40], label: 'LOCAL TRANSMISSION', cubicInterpolationMode: 'monotone' },
   ];
   public lineChartInfectionSourceLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
   public lineChartInfectionSourceOptions: (ChartOptions & { annotation: any }) = {
     responsive: true,
-    annotation: false
+    annotation: false,
+    scales: {
+      xAxes: [
+        {
+          display: false
+        }
+      ],
+      yAxes: [
+        {
+          display: false
+        }
+      ]
+    }
   };
   public lineChartInfectionSourceColors: Color[] = [
     {
@@ -157,6 +153,118 @@ export class HomeComponent implements OnInit {
       borderColor: '#ffa1b5',
       backgroundColor: 'rgba(255,161,181,0.3)',
     },
+  ];
+
+  //Confirmed card Line chart 
+  public lineChartConfirmedData: ChartDataSets[] = [
+    { data: [10, 12, 10, 8, 15, 18, 14, 17, 21, 23], label: 'CONFIRMED CASES', cubicInterpolationMode: 'monotone' }
+  ];
+  public lineChartConfirmedSourceLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartConfirmedSourceOptions: (ChartOptions & { annotation: any }) = {
+    responsive: true,
+    annotation: false,
+    scales: {
+      xAxes: [
+        {
+          display: false
+        }
+      ],
+      yAxes: [
+        {
+          display: false
+        }
+      ]
+    }
+  };
+  public lineChartConfirmedSourceColors: Color[] = [
+    {
+      borderColor: '#86c7f3',
+      backgroundColor: 'rgba(134,199,243,0.3)',
+    }
+  ];
+
+  //Hospitalized card line chart
+  public lineChartSymptomaticData: ChartDataSets[] = [
+    { data: [10, 12, 15, 17, 21, 23, 23, 25, 27, 27], label: 'CONFIRMED CASES', cubicInterpolationMode: 'monotone' }
+  ];
+  public lineChartSymptomaticSourceLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartSymptomaticSourceOptions: (ChartOptions & { annotation: any }) = {
+    responsive: true,
+    annotation: false,
+    scales: {
+      xAxes: [
+        {
+          display: false
+        }
+      ],
+      yAxes: [
+        {
+          display: false
+        }
+      ]
+    }
+  };
+  public lineChartSymptomaticSourceColors: Color[] = [
+    {
+      borderColor: '#86c7f3',
+      backgroundColor: 'rgba(134,199,243,0.3)',
+    }
+  ];
+
+  //Intensive card line chart
+  public lineChartIntensiveData: ChartDataSets[] = [
+    { data: [15, 20, 30, 8, 21, 10, 5, 17, 21, 23], label: 'CONFIRMED CASES', cubicInterpolationMode: 'monotone' }
+  ];
+  public lineChartIntensiveSourceLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartIntensiveSourceOptions: (ChartOptions & { annotation: any }) = {
+    responsive: true,
+    annotation: false,
+    scales: {
+      xAxes: [
+        {
+          display: false
+        }
+      ],
+      yAxes: [
+        {
+          display: false
+        }
+      ]
+    }
+  };
+  public lineChartIntensiveSourceColors: Color[] = [
+    {
+      borderColor: '#86c7f3',
+      backgroundColor: 'rgba(134,199,243,0.3)',
+    }
+  ];
+
+  //Discharge card line chart
+  public lineChartDischargeData: ChartDataSets[] = [
+    { data: [0, 0, 2, 1, 0, 1, 3, 0, 1, 2], label: 'CONFIRMED CASES', cubicInterpolationMode: 'monotone' }
+  ];
+  public lineChartDischargeSourceLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartDischargeSourceOptions: (ChartOptions & { annotation: any }) = {
+    responsive: true,
+    annotation: false,
+    scales: {
+      xAxes: [
+        {
+          display: false
+        }
+      ],
+      yAxes: [
+        {
+          display: false
+        }
+      ]
+    }
+  };
+  public lineChartDischargeSourceColors: Color[] = [
+    {
+      borderColor: '#86c7f3',
+      backgroundColor: 'rgba(134,199,243,0.3)',
+    }
   ];
 
 
