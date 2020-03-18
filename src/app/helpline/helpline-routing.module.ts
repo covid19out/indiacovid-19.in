62a9a12f83677090e61fd90a8d0f088150d603b1
Anand Dhage <anand.dhage@aptlogica.com>
2020-03-18 17:20:05 +0530

    Blogs design

--- src/app/helpline/helpline-routing.module.ts
+++ src/app/helpline/helpline-routing.module.ts
@@ -6,11 +6,17 @@ import { helplineComponent } from './components/helpline/helpline.component';
 import { AwarenessComponent } from './components/awareness/awareness.component';
 import { ContactInformationComponent } from './components/contact-information/contact-information.component';
 import { TestingCenterComponent } from './components/testing-center/testing-center.component';
+import { AdvisorComponent } from './components/advisor/advisor.component';
+import { GuidelinesComponent } from './components/guidelines/guidelines.component';
+import { AdditionalAdvisoryComponent } from './components/additional-advisory/additional-advisory.component';
 
 const routes: Routes = [
   { path: 'awareness',  component: AwarenessComponent },
   { path: 'contact-information',  component: ContactInformationComponent },
-  { path: 'testing-center',  component: TestingCenterComponent }
+  { path: 'testing-center',  component: TestingCenterComponent },
+  { path: 'advisor',  component: AdvisorComponent },
+  { path: 'guidelines',  component: GuidelinesComponent },
+  { path: 'additional-advisory',  component: AdditionalAdvisoryComponent }
 ];
 
 @NgModule({
--- src/app/helpline/helpline.module.ts
+++ src/app/helpline/helpline.module.ts
@@ -6,10 +6,13 @@ import { helplineComponent } from './components/helpline/helpline.component';
 import { ContactInformationComponent } from './components/contact-information/contact-information.component';
 import { AwarenessComponent } from './components/awareness/awareness.component';
 import { TestingCenterComponent } from './components/testing-center/testing-center.component';
+import { AdvisorComponent } from './components/advisor/advisor.component';
+import { GuidelinesComponent } from './components/guidelines/guidelines.component';
+import { AdditionalAdvisoryComponent } from './components/additional-advisory/additional-advisory.component';
 
 
 @NgModule({
-  declarations: [helplineComponent, ContactInformationComponent, AwarenessComponent, TestingCenterComponent],
+  declarations: [helplineComponent, ContactInformationComponent, AwarenessComponent, TestingCenterComponent, AdvisorComponent, GuidelinesComponent, AdditionalAdvisoryComponent],
   imports: [
     CommonModule,
     HelplineRoutingModule
