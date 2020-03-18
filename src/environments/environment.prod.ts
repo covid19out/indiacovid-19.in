9a7c0428023f7cc795fa2ef01b1b8055de3e4f00
Aniket Dharia (aptlogica) <aniket.dharia@aptlogica.com>
2020-03-18 22:11:39 +0530

    added firebase and other preloading updates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAM0OMduMLIiBzBGyJ0QOV32lBj4wYvf10",
+    authDomain: "india-covid-19.firebaseapp.com",
+    databaseURL: "https://india-covid-19.firebaseio.com",
+    projectId: "india-covid-19",
+    storageBucket: "india-covid-19.appspot.com",
+    messagingSenderId: "160953353240",
+    appId: "1:160953353240:web:f89cb4ee29faf379812f6d",
+    measurementId: "G-Y76YBXQDCR"
+  }
 };
