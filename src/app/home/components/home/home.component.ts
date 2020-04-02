04c918eb0f190e4a9c415a25fd082736576c6237
Aniket Dharia (aptlogica) <aniket.dharia@aptlogica.com>
2020-04-03 00:46:50 +0530

    updated text
    updated text

--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -226,7 +226,7 @@ export class HomeComponent implements OnInit {
     twttr.widgets.createTimeline(
       {
         sourceType: "list",
-        ownerScreenName: "TwitterDev",
+        ownerScreenName: "covid_19_India",
         slug: "covid_19_India"
       },
       document.getElementById("twitterTimelineWrapper")
@@ -309,7 +309,7 @@ export class HomeComponent implements OnInit {
     if (dateWiseData.length) {
       var states = _.groupBy(dateWiseData, 'state');
       var sortedStates = this.getSortedObject(states);
-      console.log(sortedStates);
+      //console.log(sortedStates);
       for (let state in sortedStates) {
         this.stateBarChartLabels.push(state);
         
